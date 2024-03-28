20ff8f066b7402c5106faee4dabab0fbb356517e
shanmugamani <shanmugamani@shravtek.com>
2024-03-28 13:54:46 +0530

    data_ingestion_added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # chest_cancer_classification using MLflow,DVC
 Chest cancer classification involves categorizing abnormalities in the chest, typically the lungs, based on imaging tests and biopsies to determine if they are cancerous and, if so, what type of cancer it is.
+
+## Workflows
+
+1. Update config.yaml
+2. Update secrets.yaml [Optional]
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline 
+8. Update the main.py
+9. Update the dvc.yaml
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 from cnnClassifier import logger
-logger.info("Hai shan,welcome to Cancer classification")
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
